e9379a0861266f06c77b58351e92e4c0f2cdf46c
hazmanaz <hamzahelwazir@yahoo.com>
2021-04-04 15:37:44 -0500

    Added window size input for SSD block matching and input validation

--- disparity_estimation.cpp
+++ disparity_estimation.cpp
@@ -14,23 +14,38 @@
 using namespace std;
 
 vector<vector<double>> parseCSV(string filename);
-void estimateDisparity(vector<vector<double>> left_img, vector<vector<double>> right_img);
+void estimateDisparity(vector<vector<double>> left_img, vector<vector<double>> right_img, int window_size);
+int SSD(vector<vector<double>> left_img, vector<vector<double>> right_img, int window_size, int i, int j, int k);
 
-int main() {
-    vector<vector<double>> view1_vec = parseCSV("view1_csv");
-    vector<vector<double>> view5_vec = parseCSV("view5_csv");
+
+int main(int argc, char* argv[]) {
+
+    int window_size;
+
+    if (argc > 1) {
+        window_size = stoi(argv[1]);
+    } else {
+        window_size = 3;
+    }
+
+    cout <<"Window Size: " << window_size << endl;
+
+    cout << "Generating disparity estimate with " << window_size << "x" << window_size << " window" << endl;
+
+    vector<vector<double>> view1_vec = parseCSV("/Users/haz/Desktop/CS 120/Module 3/Open Ended Projects/M3OEP-MPL-helwazir/view1_csv");
+    vector<vector<double>> view5_vec = parseCSV("/Users/haz/Desktop/CS 120/Module 3/Open Ended Projects/M3OEP-MPL-helwazir/view5_csv");
 
     cout << "Size: " << view1_vec.size() << " x " << view1_vec[0].size() << endl;
     cout << "Size: " << view5_vec.size() << " x " << view5_vec[0].size() << endl;
 
-    estimateDisparity(view1_vec, view5_vec);
+    estimateDisparity(view1_vec, view5_vec, window_size);
 
     return 0;
 }
 
 vector<vector<double>> parseCSV(string filename) {
 //    ifstream myFile("/Users/haz/Desktop/CS 120/Misc/MLP_maybe/" + filename);
-    ifstream myFile("./" + filename);
+    ifstream myFile(filename);
     if(!myFile.is_open()) throw std::runtime_error("Could not open file");
 
     string line, val;
@@ -53,7 +68,7 @@ vector<vector<double>> parseCSV(string filename) {
 }
 
 
-void estimateDisparity(vector<vector<double>> left_img, vector<vector<double>> right_img) {
+void estimateDisparity(vector<vector<double>> left_img, vector<vector<double>> right_img, int window_size) {
     ofstream myFile;
     myFile.open("disp_output.csv");
 
@@ -67,60 +82,76 @@ void estimateDisparity(vector<vector<double>> left_img, vector<vector<double>> r
     int MAX_DISPARITY = 75;
     int min_col, max_col;
 
+    int idx;
 
-    for (i = 2; i < rows - 1; ++i) {
-        vector<double> disp_row;
-        for (j = 2; j < cols - 1; ++j) {
+    int upper = (window_size / 2);
+    int lower = upper + 1;
 
-            if (j > MAX_DISPARITY + 1) {
+
+    for (i = lower; i < rows - upper; ++i) {
+//        vector<double> disp_row;
+
+        for (j = lower; j < cols - upper; ++j) {
+
+            if (j > MAX_DISPARITY + upper) {
                 min_col = j - MAX_DISPARITY - 1;
             } else {
-                min_col = 2;
+                min_col = lower;
             }
-            max_col = j - 1;
+            max_col = j - upper;
 
             long min_ssd = 1000000000;
             long index = 0;
 
             for (k = min_col; k < max_col; ++k) {
-
-                idx00 = left_img[i - 1][j - 1] - right_img[i - 1][k - 1];
-                idx01 = left_img[i - 1][j] - right_img[i - 1][k];
-                idx02 = left_img[i - 1][j + 1] - right_img[i - 1][k + 1];
-
-                idx10 = left_img[i][j - 1] - right_img[i][k - 1];
-                idx11 = left_img[i][j] - right_img[i][k];
-                idx12 = left_img[i][j + 1] - right_img[i][k + 1];
-
-                idx20 = left_img[i + 1][j - 1] - right_img[i + 1][k - 1];
-                idx21 = left_img[i + 1][j] - right_img[i - 1][k];
-                idx22 = left_img[i + 1][j + 1] - right_img[i + 1][k + 1];
-
-                sqr_idx00 = idx00 * idx00;
-                sqr_idx01 = idx01 * idx01;
-                sqr_idx02 = idx02 * idx02;
-
-                sqr_idx10 = idx10 * idx10;
-                sqr_idx11 = idx11 * idx11;
-                sqr_idx12 = idx12 * idx12;
-
-                sqr_idx20 = idx20 * idx20;
-                sqr_idx21 = idx21 * idx21;
-                sqr_idx22 = idx22 * idx22;
-
-                ssd = sqr_idx00 + sqr_idx01 + sqr_idx02
-                      + sqr_idx10 + sqr_idx11 + sqr_idx12
-                      + sqr_idx20 + sqr_idx21 + sqr_idx22;
+                int sum = 0;
+                for (int x = -(window_size/2); x <= window_size/2; ++x) {
+                    for (int y = -(window_size/2); y <= window_size/2; ++y) {
+                        idx = left_img[i + x][j + y] - right_img[i + x][k + y];
+                        sum += idx * idx;
+                    }
+                }
+                ssd = sum;
+//
+//
+//                idx00 = left_img[i - 1][j - 1] - right_img[i - 1][k - 1];
+//                idx01 = left_img[i - 1][j] - right_img[i - 1][k];
+//                idx02 = left_img[i - 1][j + 1] - right_img[i - 1][k + 1];
+//
+//                idx10 = left_img[i][j - 1] - right_img[i][k - 1];
+//                idx11 = left_img[i][j] - right_img[i][k];
+//                idx12 = left_img[i][j + 1] - right_img[i][k + 1];
+//
+//                idx20 = left_img[i + 1][j - 1] - right_img[i + 1][k - 1];
+//                idx21 = left_img[i + 1][j] - right_img[i - 1][k];
+//                idx22 = left_img[i + 1][j + 1] - right_img[i + 1][k + 1];
+//
+//                sqr_idx00 = idx00 * idx00;
+//                sqr_idx01 = idx01 * idx01;
+//                sqr_idx02 = idx02 * idx02;
+//
+//                sqr_idx10 = idx10 * idx10;
+//                sqr_idx11 = idx11 * idx11;
+//                sqr_idx12 = idx12 * idx12;
+//
+//                sqr_idx20 = idx20 * idx20;
+//                sqr_idx21 = idx21 * idx21;
+//                sqr_idx22 = idx22 * idx22;
+//
+//                ssd = sqr_idx00 + sqr_idx01 + sqr_idx02
+//                      + sqr_idx10 + sqr_idx11 + sqr_idx12
+//                      + sqr_idx20 + sqr_idx21 + sqr_idx22;
 
                 if (ssd < min_ssd) {
                     min_ssd = ssd;
                     index = k;
                 }
             }
+//            cout << "DONE" << endl;
             long diff = abs(j - index);
             myFile << diff << ',';
         }
         myFile << '\n';
-        cout << "Row " << i << "done" << endl;
+        cout << "Row " << i << " done" << endl;
     }
 }
--- image_handling.py
+++ image_handling.py
@@ -1,27 +1,47 @@
 import os
 import subprocess
+import platform
 from subprocess import Popen, PIPE, check_output
 import time
 import numpy as np
 import cv2
 
 
-view1_img = cv2.imread('./input_images/view1.png', 0)
-view5_img = cv2.imread('./input_images/view5.png', 0)
+view1_img = cv2.imread('input_images/view1.png', 0)
+view5_img = cv2.imread('input_images/view5.png', 0)
 
 np.savetxt('view1_csv', view1_img, delimiter=',')
 np.savetxt('view5_csv', view5_img, delimiter=',')
 
+
+# Get user input for block matching window size
+window_size = input("Please enter a window size: ")
+while int(window_size) % 2 == 0:
+    window_size = input("Window size must be odd, please enter a new window size: ")
+
 tic = time.time()
-subprocess.check_output("g++ -std=c++1y disparity_estimation.cpp", stdin=None, stderr=subprocess.STDOUT, shell=True)
-p = Popen('./a.out', shell=True, stdout=PIPE, stdin=PIPE)
-p.wait()
-os.remove("a.out")
+
+try:
+    subprocess.check_output("g++ -std=c++1y disparity_estimation.cpp", stdin=None, stderr=subprocess.STDOUT, shell=True)
+except subprocess.CalledProcessError as e:
+    print("<p>", e.output, "</p>")
+    raise SystemExit
+
+if platform.system() == 'Windows':
+    p = Popen('a.exe ' + str(window_size), shell=True, stdout=PIPE, stdin=PIPE)
+    p.wait()
+    os.remove("a.exe")
+else:
+    p = Popen(['./a.out ' + str(window_size)], shell=True, stdout=PIPE, stdin=PIPE)
+    p.wait()
+    os.remove("a.out")
+
 toc = time.time()
 
-disp_img = np.genfromtxt('disp_output.csv', dtype=np.uint8, delimiter=',')
+disparity_img = np.genfromtxt('disp_output.csv', dtype=np.uint8, delimiter=',')
 
-print("Finished in ", (toc - tic) * 1000)
-cv2.imshow("Disparity Estimation img", disp_img)
+print("Finished in", f'{toc-tic:.5g}', "seconds")
+print()
+cv2.imshow("Disparity Estimation img", disparity_img)
 cv2.waitKey()
 
