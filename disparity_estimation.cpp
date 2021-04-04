f19ab5d3097561de3e81feb06c5d1bbd545c20ed
hazmanaz <hamzahelwazir@yahoo.com>
2021-04-04 16:03:34 -0500

    Added comments

--- disparity_estimation.cpp
+++ disparity_estimation.cpp
@@ -6,64 +6,66 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
-#include <utility> // std::pair
-#include <stdexcept> // std::runtime_error
-#include <sstream> // std::stringstream
-
+#include <stdexcept>
+#include <sstream>
 using namespace std;
 
+/*
+ * Requires: The filename of the csv file containing the image intensity values
+ * Modifies: Nothing
+ * Effects: Opens the specified csv file of pixel intensities and reads the values into a two dimensional vector and
+ *          returns it.
+ */
 vector<vector<double>> parseCSV(string filename);
-void estimateDisparity(vector<vector<double>> left_img, vector<vector<double>> right_img, int window_size);
-int SSD(vector<vector<double>> left_img, vector<vector<double>> right_img, int window_size, int i, int j, int k);
 
+/*
+ * Requires: A two dimensional vector of pixel intensities present in the left image, a two dimensional vector of pixel
+ *           intensities present in the right image, and the window size to be used for SSD based block matching
+ * Modifies: disp_output.csv
+ * Effects: Uses SSD (sum of squared differences) based block matching with a user specified window size to estimate
+ *          disparity between a stereo pair of rectified images. Uses maximum disparity threshold and directional
+ *          search to minimize calculations. Minimum SSD values for each pixel coordinate are written to disp_output.csv.
+ */
+void estimateDisparity(vector<vector<double>> left_img, vector<vector<double>> right_img, int window_size);
 
 int main(int argc, char* argv[]) {
-
     int window_size;
 
+    // Get command line input it it exists, if not then default window_size to 3
     if (argc > 1) {
         window_size = stoi(argv[1]);
     } else {
         window_size = 3;
     }
 
-    cout <<"Window Size: " << window_size << endl;
-
-    cout << "Generating disparity estimate with " << window_size << "x" << window_size << " window" << endl;
-
+    // Read pixel intensities from csv files into vectors
     vector<vector<double>> view1_vec = parseCSV("/Users/haz/Desktop/CS 120/Module 3/Open Ended Projects/M3OEP-MPL-helwazir/view1_csv");
     vector<vector<double>> view5_vec = parseCSV("/Users/haz/Desktop/CS 120/Module 3/Open Ended Projects/M3OEP-MPL-helwazir/view5_csv");
 
-    cout << "Size: " << view1_vec.size() << " x " << view1_vec[0].size() << endl;
-    cout << "Size: " << view5_vec.size() << " x " << view5_vec[0].size() << endl;
-
+    // Estimate disparity between images
     estimateDisparity(view1_vec, view5_vec, window_size);
 
     return 0;
 }
 
 vector<vector<double>> parseCSV(string filename) {
-//    ifstream myFile("/Users/haz/Desktop/CS 120/Misc/MLP_maybe/" + filename);
-    ifstream myFile(filename);
-    if(!myFile.is_open()) throw std::runtime_error("Could not open file");
-
     string line, val;
     vector<vector<double>> img_vec;
 
+    ifstream myFile(filename);
+    if(!myFile.is_open()) throw std::runtime_error("Could not open file");
+
     while (myFile.peek() != EOF) {
         vector<double> row;
-
         getline(myFile, line, '\n');
         stringstream s(line);
-
         while (getline(s, val, ',')) {
             row.push_back(stof(val));
         }
         img_vec.push_back(row);
     }
-
     myFile.close();
+
     return img_vec;
 }
 
@@ -73,26 +75,18 @@ void estimateDisparity(vector<vector<double>> left_img, vector<vector<double>> r
     myFile.open("disp_output.csv");
 
     vector<vector<double>> disp_map;
-    long i, j, k;
-    long idx00, idx01, idx02, idx10, idx11, idx12, idx20, idx21, idx22;
-    long sqr_idx00, sqr_idx01, sqr_idx02, sqr_idx10, sqr_idx11, sqr_idx12, sqr_idx20, sqr_idx21, sqr_idx22;
-    long ssd;
-    long rows = left_img.size();
-    long cols = left_img[0].size();
-    int MAX_DISPARITY = 75;
+    int i, j, k, x, y;
+    int ssd, idx;
     int min_col, max_col;
-
-    int idx;
-
+    int MAX_DISPARITY = 75;  // Max disparity previously calculated by finding the maximum disparity in the ground truth of left_img
+    int rows = left_img.size();
+    int cols = left_img[0].size();
     int upper = (window_size / 2);
     int lower = upper + 1;
 
-
-    for (i = lower; i < rows - upper; ++i) {
-//        vector<double> disp_row;
-
-        for (j = lower; j < cols - upper; ++j) {
-
+    for (i = lower; i < rows - upper; ++i) {  // For each row in the image
+        for (j = lower; j < cols - upper; ++j) {  // For each column in the image
+            // Directional search and max disparity thresholding
             if (j > MAX_DISPARITY + upper) {
                 min_col = j - MAX_DISPARITY - 1;
             } else {
@@ -100,56 +94,29 @@ void estimateDisparity(vector<vector<double>> left_img, vector<vector<double>> r
             }
             max_col = j - upper;
 
-            long min_ssd = 1000000000;
-            long index = 0;
+            int min_ssd = 1000000000;
+            int index = 0;
 
-            for (k = min_col; k < max_col; ++k) {
-                int sum = 0;
-                for (int x = -(window_size/2); x <= window_size/2; ++x) {
-                    for (int y = -(window_size/2); y <= window_size/2; ++y) {
+            for (k = min_col; k < max_col; ++k) {  // For each pixel left of j
+                ssd = 0;
+                // Calculate the SSD between two windows (sub images) in left_img and right_img
+                for (x = -(window_size/2); x <= window_size/2; ++x) {
+                    for (y = -(window_size/2); y <= window_size/2; ++y) {
                         idx = left_img[i + x][j + y] - right_img[i + x][k + y];
-                        sum += idx * idx;
+                        ssd += idx * idx;
                     }
                 }
-                ssd = sum;
-//
-//
-//                idx00 = left_img[i - 1][j - 1] - right_img[i - 1][k - 1];
-//                idx01 = left_img[i - 1][j] - right_img[i - 1][k];
-//                idx02 = left_img[i - 1][j + 1] - right_img[i - 1][k + 1];
-//
-//                idx10 = left_img[i][j - 1] - right_img[i][k - 1];
-//                idx11 = left_img[i][j] - right_img[i][k];
-//                idx12 = left_img[i][j + 1] - right_img[i][k + 1];
-//
-//                idx20 = left_img[i + 1][j - 1] - right_img[i + 1][k - 1];
-//                idx21 = left_img[i + 1][j] - right_img[i - 1][k];
-//                idx22 = left_img[i + 1][j + 1] - right_img[i + 1][k + 1];
-//
-//                sqr_idx00 = idx00 * idx00;
-//                sqr_idx01 = idx01 * idx01;
-//                sqr_idx02 = idx02 * idx02;
-//
-//                sqr_idx10 = idx10 * idx10;
-//                sqr_idx11 = idx11 * idx11;
-//                sqr_idx12 = idx12 * idx12;
-//
-//                sqr_idx20 = idx20 * idx20;
-//                sqr_idx21 = idx21 * idx21;
-//                sqr_idx22 = idx22 * idx22;
-//
-//                ssd = sqr_idx00 + sqr_idx01 + sqr_idx02
-//                      + sqr_idx10 + sqr_idx11 + sqr_idx12
-//                      + sqr_idx20 + sqr_idx21 + sqr_idx22;
 
+                // If the current ssd is less than min_ssd update min_ssd and set index = k
                 if (ssd < min_ssd) {
                     min_ssd = ssd;
                     index = k;
                 }
             }
-//            cout << "DONE" << endl;
-            long diff = abs(j - index);
-            myFile << diff << ',';
+
+            // Calculate the disparity between the same object in the two images and write it to disp_output.csv
+            int disparity = abs(j - index);
+            myFile << disparity << ',';
         }
         myFile << '\n';
         cout << "Row " << i << " done" << endl;
